16d2a990ecf52cd0b3a030caf631e187e78767db
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-04 01:27:34 +0700

    update keystore and service mapping

--- src/main/java/com/management_system/api_gateway/ConstantValues.java
+++ src/main/java/com/management_system/api_gateway/ConstantValues.java
@@ -3,7 +3,7 @@ package com.management_system.api_gateway;
 public class ConstantValues {
     public static String AUTHEN_SERVICE = "authentication-service";
     public static String AUTHEN_SERVICE_PATTERN = "/authentication-service/**";
-    public static String AUTHEN_SERVICE_URI = "lb://authentication-service";
+    public static String AUTHEN_SERVICE_URI = "https://localhost:8080";
 
     public static String INGREDIENT_SERVICE = "ingredient-service";
 }
--- src/main/java/com/management_system/api_gateway/GatewayConfig.java
+++ src/main/java/com/management_system/api_gateway/GatewayConfig.java
@@ -1,5 +1,7 @@
 package com.management_system.api_gateway;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +14,25 @@ public class GatewayConfig {
         return builder
                 .routes()
                 .route(ConstantValues.AUTHEN_SERVICE, r ->
-                        r.path(ConstantValues.AUTHEN_SERVICE_PATTERN).uri(ConstantValues.AUTHEN_SERVICE_URI))
+                        r.path(ConstantValues.AUTHEN_SERVICE_PATTERN)
+                                .filters(f -> f.removeRequestHeader("Forwarded"))
+                                .uri(ConstantValues.AUTHEN_SERVICE_URI)
+                )
                 .build();
+    }
+
+
+    @Bean
+    public NettyReactiveWebServerFactory nettyFactory(ServerProperties serverProperties) {
+        var maxInBytes = (int) serverProperties.getMaxHttpRequestHeaderSize().toBytes();
+        var factory = new NettyReactiveWebServerFactory();
+        factory.addServerCustomizers(
+                server -> server.httpRequestDecoder(
+                        reqDecoder -> reqDecoder
+                                .maxInitialLineLength(maxInBytes)
+                                .maxHeaderSize(maxInBytes)
+                )
+        );
+        return factory;
     }
 }
